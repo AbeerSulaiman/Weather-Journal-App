2f8c65d6e0cb4a002b01f40711935fcb70c0a31c
abeeralbawardi <albawardi.as@gmail.com>
2022-11-14 21:49:22 +0300

    set up server file & app file

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = [];
 
 // Require Express to run server and routes
+const express = require("express");
+
+/* Dependencies */
+const bodyParser = require("body-parser");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,32 +16,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
-//comments: TODO 
-// Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
+// Set up and Spin up the server
+const port = 8000;
+app.listen(port, listening);
+function listening(){
+    console.log(`running on localhost: ${port}`);
+};
 
-// Start up an instance of app
+// POST route
+app.post('/add', addData);
 
-/* Dependencies */
-/* Middleware*/
-
-//Here we are configuring express to use body-parser as middle-ware.
-// Cors for cross origin allowance
-
-// Initialize the main project folder
-
-// Spin up the server
-// Callback to debug
+function addData(req, res) {
+    console.log(req.body);
+    userData = {
+        date: req.body.date,
+        temp: req.body.temp,
+        content: req.body.content
+    }
+    projectData.push(userData);
+}
 
 // Initialize all route with a callback function
+app.get('/all', getData);
 
 // Callback function to complete GET '/all'
+function getData(req, res) {
+  res.send(projectData);
+  projectData =[];
+}
 
-// Post Route
 
-
-// Setup Server
--- website/app.js
+++ website/app.js
@@ -1,19 +1,75 @@
 /* Global Variables */
+const baseUrl = "https://api.openweathermap.org/data/2.5/forecast?zip=";
+const apiKey = "&appid=392db0addd606b4e95b31ba9a187e87d";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-
-//comments: TODO
-// Personal API Key for OpenWeatherMap API
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
 // Event listener to add function to existing HTML DOM element
+const generateBtn = document.getElementById("generate");
+generateBtn.addEventListener("click", performAction);
 
 /* Function called by event listener */
+function performAction(e) {
+  e.preventDefault();
+  const zip = document.getElementById("zip").value;
+  const feelings = document.getElementById("feelings").value;
+
+  getWeatherData(baseUrl, zip, apiKey).then(function (data) {
+    // add data to POST request
+    console.log(data);
+    postData("/add", {
+      temp: data.list[0].main.temp,
+      date: newDate,
+      content: feelings,
+    });
+    updateUI();
+  });
+}
 
 /* Function to GET Web API Data*/
+const getWeatherData = async (baseUrl, zip, apiKey) => {
+  const res = await fetch(baseUrl + zip + apiKey);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
 /* Function to POST data */
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-/* Function to GET Project Data */
+const userDate = document.getElementById("date");
+const userTemp = document.getElementById("temp");
+const userContent = document.getElementById("content");
+// console.log( userDate, userTemp, userContent);
+const updateUI = async () => {
+  const request = await fetch("/all");
+  try {
+    const userData = await request.json();
+    userDate.innerHTML = userData[0].date;
+    userTemp.innerHTML = userData[0].temp;
+    userContent.innerHTML = userData[0].content;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
